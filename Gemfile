2231a76c197f46e59c599dc4997ed8a55033dea4
swkeylon <swkeylon@gmail.com>
2023-10-07 00:45:11 +0000

    First attempt at getting through the tutorial, no luck so far

--- Gemfile
+++ Gemfile
@@ -76,3 +76,7 @@ group :test do
 end
 
 gem "yaml_db"
+
+gem 'devise'
+gem 'omniauth', "~> 1.9.1"
+gem 'omniauth-google-oauth2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root "members#index"
+  root to: 'dashboards#show'
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
 
   match 'about', to: "main#about", via: :get
   match 'hello', to: "main#hello", via: :get
