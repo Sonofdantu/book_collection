c76cb95633e461671d39b003ce0a7f1dda1af6d8
Rohan889 <rohan8.talluri@gmail.com>
2023-10-16 12:35:14 -0500

    Minor Changes to Usability while adding member view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
-
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
+  before_action :set_current_member
+
+  private
+
+  def set_current_member
+    @current_member = Member.find_by(email: current_admin.email) if admin_signed_in?
+  end
 end
