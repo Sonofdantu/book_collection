cc64e9f4f2cd60781da543742d4334bbf66e8695
Rohan889 <rohan8.talluri@gmail.com>
2023-10-24 08:35:41 -0500

    Added a header and some different views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_admin!
   before_action :set_current_member
+  helper_method :current_member
 
   private
 
   def set_current_member
     @current_member = Member.find_by(email: current_admin.email) if admin_signed_in?
+  end
+
+  def current_member
+    @current_member
   end
 end
