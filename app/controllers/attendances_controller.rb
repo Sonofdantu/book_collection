4e33fb96e294ae86d524b4ebd31ca2f8aa48c04f
root <root@localhost>
2023-11-07 01:28:11 +0000

    not done yet but added functionality for showing events and the attendance

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,6 +4,7 @@ class AttendancesController < ApplicationController
   # GET /attendances or /attendances.json
   def index
     @attendances = Attendance.all
+    @events = Event.all
   end
 
   # GET /attendances/1 or /attendances/1.json
